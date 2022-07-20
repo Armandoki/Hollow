c96140637256235a68f0d97f5a7d35e2db329b30
Armandoki <arm.neira@duocuc.cl>
2022-07-20 13:20:37 -0400

    Se agregaron efectos de sonido y música de fondo

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+//Variables Jeje
 let respuestaArray = []
 let respuestaString = ''
 let respuestaNumber = 0
@@ -6,8 +7,21 @@ let operacionActual = crearOperacion()
 let puntosReales = 0
 let mejorRacha = 0
 
+//Sonidos y Configuracion de Sonidos
+const victoria = new Audio('static/sounds/hero_parry.wav')
+const derrota = new Audio('static/sounds/hero_damage.wav')
+const fondo = new Audio('static/sounds/dirtmouth.wav')
+
+victoria.volume = 0.7
+derrota.volume = 0.7
+
+fondo.volume = 0.3
+fondo.loop = true
+fondo.play()
+
 mostrarInfomacion()
 
+//Funciones
 function actualizarDatos(){
     respuestaString = respuestaArray.join('')
     respuestaNumber = parseInt(respuestaString)
@@ -37,10 +51,16 @@ function crearOperacion(){
 
 function ingresarNumero(numeroIngresado){
     if(numeroIngresado==10){
-        respuestaArray.push('-')
+        if(respuestaArray.length < 10)
+        {
+            respuestaArray.push('-')
+        }  
     }
     else{
-        respuestaArray.push(numeroIngresado)
+        if(respuestaArray.length < 10)
+        {
+            respuestaArray.push(numeroIngresado)
+        }
     }
     mostrarInfomacion()
 }
@@ -132,16 +152,28 @@ function mostrarInfomacion(){
 
 function cambiarColores(parametro){
     if(parametro=='BIEN'){ 
+        victoria.play()
+
         document.getElementById('cuerpo').classList.replace('fondo-cuerpo-normal', 'fondo-cuerpo-bien')
         setTimeout(function(){
             document.getElementById('cuerpo').classList.replace('fondo-cuerpo-bien', 'fondo-cuerpo-normal')
         },100)
     }
     else{
+        derrota.play()
+
         document.getElementById('cuerpo').classList.replace('fondo-cuerpo-normal', 'fondo-cuerpo-mal')
         setTimeout(function(){
             document.getElementById('cuerpo').classList.replace('fondo-cuerpo-mal', 'fondo-cuerpo-normal')
         },100)
-    }
-
+    }    
 }
+
+function detenerFondo(){
+    if(fondo.paused){
+        fondo.play()
+    }
+    else{
+        fondo.pause()
+    }
+}   
